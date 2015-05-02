a7199f19e38b75d606cf815180a9f72cd2e2e772
Maxime <Maxime@macbook.home>
2015-05-02 18:54:30 +0200

    Ajout de la rotation

--- eternity/src/main/java/fr/esiea/glpoo/JeuFrame.java
+++ eternity/src/main/java/fr/esiea/glpoo/JeuFrame.java
@@ -4,27 +4,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 import java.util.Scanner;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.DropMode;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
+import java.awt.*
+;
 
-
-public class JeuFrame extends JFrame implements ActionListener{
+public class JeuFrame extends JFrame{
 	
 	 private JPanel contentPane;
 	 public JTable table;
 	 ImageIcon icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9, icon10, icon11, icon12, icon13, icon14, icon15, icon16;
 	 JSplitPane splitPane;
 	 DefaultTableModel model;
+	 JLabel label;
+	 int angle = 90;
+	 ImageIcon iconrot;
 	 
 	public static void main(String[] args) {
 	        try {
@@ -47,7 +56,7 @@ public class JeuFrame extends JFrame implements ActionListener{
     
     splitPane = new JSplitPane();
     splitPane.setResizeWeight(0.5);
-    
+  
     icon1 = new ImageIcon("/Users/Maxime/git/GLPOOO/eternity/src/images/1_1.png");
      icon2 = new ImageIcon("/Users/Maxime/git/GLPOOO/eternity/src/images/1_2.png");
      icon3 = new ImageIcon("/Users/Maxime/git/GLPOOO/eternity/src/images/1_3.png");
@@ -98,9 +107,20 @@ public class JeuFrame extends JFrame implements ActionListener{
     contentPane.add(splitPane, BorderLayout.CENTER);
     
     JButton bouton = new JButton("Modification");
-    bouton.addActionListener(this); 
-    splitPane.setRightComponent(bouton);
-    contentPane.add(splitPane, BorderLayout.CENTER);
+    bouton.addActionListener(new BoutonListener()); 
+    JButton rotate = new JButton("Rotation");
+    rotate.addActionListener(new Bouton2Listener()); 
+    Box container = Box.createVerticalBox();
+    Box currentLine = null;  
+
+    currentLine = Box.createHorizontalBox();
+    currentLine.add(bouton);
+    container.add(currentLine);
+    
+    currentLine = Box.createHorizontalBox();
+    currentLine.add(rotate);
+    container.add(currentLine);
+    splitPane.setRightComponent(container);
 	}
 	
 	public void exchange(int departrow, int departcol, int arriverow, int arrivecol){
@@ -108,29 +128,69 @@ public class JeuFrame extends JFrame implements ActionListener{
 		table.setValueAt(table.getValueAt(departrow, departcol),arriverow,arrivecol);
 		table.setValueAt(test,departrow,departcol);
 	}
-
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-	    System.out.println("Veuillez saisir departrow :");
-	    String strdr = sc.nextLine();
-	    System.out.println("Vous avez saisi : " + strdr);
-	    System.out.println("Veuillez saisir departcol :");
-	    String strdc = sc.nextLine();
-	    System.out.println("Vous avez saisi : " + strdc);
-	    System.out.println("Veuillez saisir arrivecol :");
-	    String strac = sc.nextLine();
-	    System.out.println("Vous avez saisi : " + strac);
-	    System.out.println("Veuillez saisir arriverow :");
-	    String strar = sc.nextLine();
-	    System.out.println("Vous avez saisi : " + strar);
-	    int departrow; int departcol; int arriverow; int arrivecol;
-	    departrow = Integer.parseInt(strdr);
-	    departcol = Integer.parseInt(strdc);
-	    arriverow = Integer.parseInt(strar);
-	    arrivecol = Integer.parseInt(strac);
-	    exchange(departrow, departcol, arriverow, arrivecol);
-	    model.fireTableDataChanged();
-		
+	
+	public void rotate(int row, int column){
+		int w = ((ImageIcon) table.getValueAt(row, column)).getIconWidth();
+        int h = ((ImageIcon) table.getValueAt(row, column)).getIconHeight();
+        int type = BufferedImage.TYPE_INT_RGB;  // other options, see api
+        BufferedImage image = new BufferedImage(h, w, type);
+        Graphics2D g2 = image.createGraphics();
+        double x = (h - w)/2.0;
+        double y = (w - h)/2.0;
+        AffineTransform at = AffineTransform.getTranslateInstance(x, y);
+        at.rotate(Math.toRadians(angle), w/2.0, h/2.0);
+        g2.drawImage(((ImageIcon) table.getValueAt(row, column)).getImage(), at, table);
+        g2.dispose();
+        iconrot = new ImageIcon(image);
+        model.fireTableDataChanged();  
 	}
+
+	
+	class BoutonListener implements ActionListener{
+	    //Redéfinition de la méthode actionPerformed()
+	    public void actionPerformed(ActionEvent arg0) {
+	    	Scanner sc = new Scanner(System.in);
+		    System.out.println("Veuillez saisir departrow :");
+		    String strdr = sc.nextLine();
+		    System.out.println("Vous avez saisi : " + strdr);
+		    System.out.println("Veuillez saisir departcol :");
+		    String strdc = sc.nextLine();
+		    System.out.println("Vous avez saisi : " + strdc);
+		    System.out.println("Veuillez saisir arrivecol :");
+		    String strac = sc.nextLine();
+		    System.out.println("Vous avez saisi : " + strac);
+		    System.out.println("Veuillez saisir arriverow :");
+		    String strar = sc.nextLine();
+		    System.out.println("Vous avez saisi : " + strar);
+		    int departrow; int departcol; int arriverow; int arrivecol;
+		    departrow = Integer.parseInt(strdr);
+		    departcol = Integer.parseInt(strdc);
+		    arriverow = Integer.parseInt(strar);
+		    arrivecol = Integer.parseInt(strac);
+		    exchange(departrow, departcol, arriverow, arrivecol);
+		    model.fireTableDataChanged();    
+	    }
+	  }
+	      
+	  //Classe écoutant notre second bouton
+	  class Bouton2Listener implements ActionListener{
+	    //Redéfinition de la méthode actionPerformed()
+	    public void actionPerformed(ActionEvent e) {
+	    	Scanner sc = new Scanner(System.in);
+		    System.out.println("Veuillez saisir la ligne :");
+		    String strdr = sc.nextLine();
+		    System.out.println("Veuillez saisir la colonne :");
+		    String strdc = sc.nextLine();
+		    int row, col; 
+		    row = Integer.parseInt(strdr);
+		    col = Integer.parseInt(strdc);
+	      String ac = e.getActionCommand();
+	        if(ac.equals("CCW"))
+	            angle = -90;
+	        else
+	            angle = 90;     // degrees clockwise
+	        rotate(row,col);
+	        table.setValueAt(iconrot,row,col);
+	    }
+	  }      
 }
